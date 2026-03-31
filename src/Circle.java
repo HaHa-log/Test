357bd1fe06473f8f6514614f4fd02008b3408e78
VuongHang1102 <25020144@vnu.edu.vn>
2026-03-31 08:00:23 +0700

    Bai1
    Tuan4

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle extends Shape {
+    public Circle(int x, int y) {
+        super(x,y);
+    }
+
+    public void draw() {
+        System.out.println("Ve hinh tron tai (" + x + ", " + y + ")" );
+    }
+    public void erase() {
+        System.out.println("Xoa hinh tron tai (" + x + ", " + y + ")" );
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,19 @@
+abstract class Shape {
+    protected int x,y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void draw();
+    public abstract void erase();
+
+    public void moveTo(int newX, int newY) {
+        erase();
+        x = newX;
+        y =newY;
+        draw();
+    }
+
+}
